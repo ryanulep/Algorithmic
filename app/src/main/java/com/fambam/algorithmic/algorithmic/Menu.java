41b0500f1cbe0c0a0541ef2c4a637bbb2091581c
mschaffrath <mscha008@ucr.edu>
2017-11-09 22:45:58 -0800

    Added logout for floating action button. User stays logged in after app close and reopen (this is good).

--- app/src/main/java/com/fambam/algorithmic/algorithmic/Menu.java
+++ app/src/main/java/com/fambam/algorithmic/algorithmic/Menu.java
@@ -1,11 +1,17 @@
 package com.fambam.algorithmic.algorithmic;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Menu extends AppCompatActivity {
 
@@ -21,9 +27,32 @@ public class Menu extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                AlertDialog.Builder builder;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    builder = new AlertDialog.Builder(Menu.this
+                            , android.R.style.Theme_Material_Dialog_Alert);
+                } else {
+                    builder = new AlertDialog.Builder(Menu.this
+                    );
+                }
+                builder.setMessage("Would you like to sign out?")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                FirebaseAuth.getInstance().signOut();
+                                startActivity(new Intent(Menu.this, UserLogin.class));
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // do nothing
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
                 //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 //        .setAction("Action", null).show();
             }
         });
     }
 }
+
