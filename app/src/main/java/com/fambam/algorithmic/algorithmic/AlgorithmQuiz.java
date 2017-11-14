72b931e9ab10604d3642cc925af88f52d65b73ad
mschaffrath <mscha008@ucr.edu>
2017-11-13 17:41:23 -0800

    Quiz nearly complete. TODO: 1. exit button 2. score in database

--- app/src/main/java/com/fambam/algorithmic/algorithmic/AlgorithmQuiz.java
+++ app/src/main/java/com/fambam/algorithmic/algorithmic/AlgorithmQuiz.java
@@ -1,19 +1,17 @@
 package com.fambam.algorithmic.algorithmic;
 
 import android.content.res.Resources;
-import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
-import java.util.Vector;
 
 public class AlgorithmQuiz extends AppCompatActivity {
 
@@ -23,6 +21,13 @@ public class AlgorithmQuiz extends AppCompatActivity {
     private Button tButton4;
 
     private TextView questionText;
+
+    private String currentAns;
+    // Colors for buttons
+    int defaultC = 0xFFFFAAAA;
+    int selectedIncorrectC = 0xFFFF0000;
+    int selectedCorrectC = 0xAAAAFFFF;
+
     //FirebaseDatabase database = FirebaseDatabase.getInstance();
     //String userUID = FirebaseAuth.getInstance().getCurrentUser().getUid();
     //DatabaseReference dRef = database.getReference(userUID);
@@ -56,7 +61,6 @@ public class AlgorithmQuiz extends AppCompatActivity {
 
         Resources res = getResources();
         int randIndex = new Random().nextInt(3);
-        String currentAns;
         if (subject.equals("bubble")){
             String[][] qList = {res.getStringArray(R.array.b_q1),res.getStringArray(R.array.b_q2),res.getStringArray(R.array.b_q3)};
             String[] currentQ = qList[randIndex];
@@ -85,6 +89,7 @@ public class AlgorithmQuiz extends AppCompatActivity {
                 bList.get(i-1).setText(ansList.get(randIndex));
                 ansList.remove(randIndex);
             }
+            currentAns = currentQ[1];
         }
         else if (subject.equals("insertion")){
             String[][] qList = {res.getStringArray(R.array.i_q1),res.getStringArray(R.array.i_q2),res.getStringArray(R.array.i_q3)};
@@ -99,6 +104,7 @@ public class AlgorithmQuiz extends AppCompatActivity {
                 bList.get(i - 1).setText(ansList.get(randIndex));
                 ansList.remove(randIndex);
             }
+            currentAns = currentQ[1];
         }
         else if (subject.equals("ls")){
             String[][] qList = {res.getStringArray(R.array.ls_q1),res.getStringArray(R.array.ls_q2),res.getStringArray(R.array.ls_q3)};
@@ -113,71 +119,96 @@ public class AlgorithmQuiz extends AppCompatActivity {
                 bList.get(i-1).setText(ansList.get(randIndex));
                 ansList.remove(randIndex);
             }
+            currentAns = currentQ[1];
         }
 
-        tButton1.setBackgroundColor(0xFFFFAAAA);
-        tButton2.setBackgroundColor(0xFFFFAAAA);
-        tButton3.setBackgroundColor(0xFFFFAAAA);
-        tButton4.setBackgroundColor(0xFFFFAAAA);
+        tButton1.setBackgroundColor(defaultC);
+        tButton2.setBackgroundColor(defaultC);
+        tButton3.setBackgroundColor(defaultC);
+        tButton4.setBackgroundColor(defaultC);
 
         //mAuth = FirebaseAuth.getInstance();
 
         tButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tButton1.setBackgroundColor(0xFFFF0000);
-                tButton2.setBackgroundColor(0xFFFFAAAA);
-                tButton3.setBackgroundColor(0xFFFFAAAA);
-                tButton4.setBackgroundColor(0xFFFFAAAA);
+                tButton1.setBackgroundColor(selectedIncorrectC);
+                tButton2.setBackgroundColor(defaultC);
+                tButton3.setBackgroundColor(defaultC);
+                tButton4.setBackgroundColor(defaultC);
                 tB1Pressed();
             }
         });
         tButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tButton1.setBackgroundColor(0xFFFFAAAA);
-                tButton2.setBackgroundColor(0xFFFF0000);
-                tButton3.setBackgroundColor(0xFFFFAAAA);
-                tButton4.setBackgroundColor(0xFFFFAAAA);
+                tButton1.setBackgroundColor(defaultC);
+                tButton2.setBackgroundColor(selectedIncorrectC);
+                tButton3.setBackgroundColor(defaultC);
+                tButton4.setBackgroundColor(defaultC);
                 tB2Pressed();
             }
         });
         tButton3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tButton1.setBackgroundColor(0xFFFFAAAA);
-                tButton2.setBackgroundColor(0xFFFFAAAA);
-                tButton3.setBackgroundColor(0xFFFF0000);
-                tButton4.setBackgroundColor(0xFFFFAAAA);
+                tButton1.setBackgroundColor(defaultC);
+                tButton2.setBackgroundColor(defaultC);
+                tButton3.setBackgroundColor(selectedIncorrectC);
+                tButton4.setBackgroundColor(defaultC);
                 tB3Pressed();
             }
         });
         tButton4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tButton1.setBackgroundColor(0xFFFFAAAA);
-                tButton2.setBackgroundColor(0xFFFFAAAA);
-                tButton3.setBackgroundColor(0xFFFFAAAA);
-                tButton4.setBackgroundColor(0xFFFF0000);
+                tButton1.setBackgroundColor(defaultC);
+                tButton2.setBackgroundColor(defaultC);
+                tButton3.setBackgroundColor(defaultC);
+                tButton4.setBackgroundColor(selectedIncorrectC);
                 tB4Pressed();
             }
         });
     }
 
     private void tB1Pressed() {
-        //if( tButton1.getText().equals(R.string.B_Q1_Ans));
-
+        if (tButton1.getText().equals(currentAns)){
+            tButton1.setBackgroundColor(selectedCorrectC);
+            createExitButton();
+        }
         //dRef.child("child1").setValue("Muh Vahlyews");
     }
     private void tB2Pressed() {
-
+        if (tButton2.getText().equals(currentAns)){
+            tButton2.setBackgroundColor(selectedCorrectC);
+            createExitButton();
+        }
     }
     private void tB3Pressed() {
-
+        if (tButton3.getText().equals(currentAns)){
+            tButton3.setBackgroundColor(selectedCorrectC);
+            createExitButton();
+        }
     }
     private void tB4Pressed() {
-
+        if (tButton4.getText().equals(currentAns)){
+            tButton4.setBackgroundColor(selectedCorrectC);
+            createExitButton();
+        }
     }
-
+    private void createExitButton(){
+        ImageButton exitB = new ImageButton(this);
+        exitB.setId(1);
+        exitB.setImageResource(R.drawable.right_arrow);
+        ConstraintLayout currentLayout = this.findViewById(R.id.quiz);
+        currentLayout.addView(exitB);
+        ConstraintSet exitBC = new ConstraintSet();
+        exitBC.clone(currentLayout);
+        exitBC.centerVertically(1, ConstraintSet.PARENT_ID);
+        exitBC.connect(1, ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT);
+        exitBC.constrainWidth(1, 200);
+        exitBC.constrainHeight(1, 400);
+        exitBC.applyTo(currentLayout);
+    }
 
 }
