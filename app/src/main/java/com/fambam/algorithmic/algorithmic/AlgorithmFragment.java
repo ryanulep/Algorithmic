076904733fe2e75178791c37df769199604ec6e7
gprice1129 <gprice1129@gmail.com>
2017-11-15 16:33:55 -0800

    Added an automatic highlighting feature based on index tracking

--- app/src/main/java/com/fambam/algorithmic/algorithmic/AlgorithmFragment.java
+++ app/src/main/java/com/fambam/algorithmic/algorithmic/AlgorithmFragment.java
@@ -21,7 +21,6 @@ public class AlgorithmFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
