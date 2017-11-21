5e6241b082d442cf7539b2aec4bdf2a808e90b43
Ryan Ulep <rulep001@ucr.edu>
2017-11-20 20:10:57 -0800

    Added summary images

--- app/src/main/java/com/fambam/algorithmic/algorithmic/AlgorithmSummary.java
+++ app/src/main/java/com/fambam/algorithmic/algorithmic/AlgorithmSummary.java
@@ -1,10 +1,7 @@
 package com.fambam.algorithmic.algorithmic;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.ImageView;
 
 public class AlgorithmSummary extends AppCompatActivity {
@@ -16,7 +13,7 @@ public class AlgorithmSummary extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_algorithm_summary);
 
-        image = findViewById(R.id.simmaryImageView);
+        image = findViewById(R.id.summaryImageView);
 
         String subject;
         Bundle extras = getIntent().getExtras();
@@ -28,16 +25,16 @@ public class AlgorithmSummary extends AppCompatActivity {
         }
 
         if (subject.equals("bubble")){
-            image.setImageResource(R.drawable.cs_summary);
+            image.setImageResource(R.drawable.bubble_summary);
         }
         else if (subject.equals("selection")){
-            image.setImageResource(R.drawable.cs_summary);
+            image.setImageResource(R.drawable.selection_summary);
         }
         else if (subject.equals("insertion")){
-            image.setImageResource(R.drawable.cs_summary);
+            image.setImageResource(R.drawable.insertion_summary);
         }
         else if (subject.equals("ls")) {
-            image.setImageResource(R.drawable.cs_summary);
+            image.setImageResource(R.drawable.ls_summary);
         }
     }
 }
