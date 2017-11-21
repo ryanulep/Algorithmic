a1aca6f13770730d99b5e7dfabef99550029c79c
mschaffrath <mscha008@ucr.edu>
2017-11-20 19:23:46 -0800

    Stuff

--- app/src/main/java/com/fambam/algorithmic/algorithmic/AlgorithmSummary.java
+++ app/src/main/java/com/fambam/algorithmic/algorithmic/AlgorithmSummary.java
@@ -5,13 +5,39 @@ import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+
 public class AlgorithmSummary extends AppCompatActivity {
+
+    private ImageView image;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_algorithm_summary);
 
+        image = findViewById(R.id.simmaryImageView);
 
+        String subject;
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            subject = extras.getString("subject" );
+        }
+        else {
+            subject = "extras were NULL";
+        }
 
+        if (subject.equals("bubble")){
+            image.setImageResource(R.drawable.MySummaryPic);
+        }
+        else if (subject.equals("selection")){
+            image.setImageResource(R.drawable.MySummaryPic);
+        }
+        else if (subject.equals("insertion")){
+            image.setImageResource(R.drawable.MySummaryPic);
+        }
+        else if (subject.equals("insertion")) {
+            image.setImageResource(R.drawable.MySummaryPic);
+        }
     }
 }
