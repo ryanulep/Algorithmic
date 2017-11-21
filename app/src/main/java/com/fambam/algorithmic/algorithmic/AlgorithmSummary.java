af0d5527481c318d1862de0b51ce15a67805ba8f
mschaffrath <mscha008@ucr.edu>
2017-11-21 13:53:38 -0800

    Testing database features. Currently Incomplete.

--- app/src/main/java/com/fambam/algorithmic/algorithmic/AlgorithmSummary.java
+++ app/src/main/java/com/fambam/algorithmic/algorithmic/AlgorithmSummary.java
@@ -2,19 +2,24 @@ package com.fambam.algorithmic.algorithmic;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AlgorithmSummary extends AppCompatActivity {
 
     private ImageView image;
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    String userUID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-    DatabaseReference dRef = database.getReference(userUID);
+    String UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    DatabaseReference dRef = database.getReference(UID);
+    String flags;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,21 +37,48 @@ public class AlgorithmSummary extends AppCompatActivity {
             subject = "extras were NULL";
         }
 
+        dRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                flags = dataSnapshot.child(UID).getValue(userData.class).getFlags();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Could not read value
+                // User logger??
+            }
+        });
+
+
         if (subject.equals("bubble")){
             image.setImageResource(R.drawable.cs_summary);
-            dRef.child("bSummary").setValue("1");
+
+//            StringBuilder flagUpdate = new StringBuilder(flags);
+//            flagUpdate.setCharAt(0, '1');
+//            dRef.child("flags").setValue(flagUpdate);
         }
         else if (subject.equals("selection")){
-            image.setImageResource(R.drawable.selection_summary);
-            dRef.child("sSummary").setValue("1");
+            image.setImageResource(R.drawable.cs_summary);
+
+//            StringBuilder flagUpdate = new StringBuilder(flags);
+//            flagUpdate.setCharAt(4, '1');
+//            dRef.child("flags").setValue(flagUpdate);
         }
         else if (subject.equals("insertion")){
-            image.setImageResource(R.drawable.insertion_summary);
-            dRef.child("iSummary").setValue("1");
+            image.setImageResource(R.drawable.cs_summary);
+
+//            StringBuilder flagUpdate = new StringBuilder(flags);
+//            flagUpdate.setCharAt(8, '1');
+//            dRef.child("flags").setValue(flagUpdate);
         }
         else if (subject.equals("ls")) {
-            image.setImageResource(R.drawable.ls_summary);
-            dRef.child("lsSummary").setValue("1");
+            image.setImageResource(R.drawable.cs_summary);
+
+//            StringBuilder flagUpdate = new StringBuilder(flags);
+//            flagUpdate.setCharAt(12, '1');
+//            dRef.child("flags").setValue(flagUpdate);
         }
     }
 }
